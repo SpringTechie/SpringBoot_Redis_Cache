a45c4e56cff27537cdb97fc79befd648f595a0fa
springtechie <springframeworkdeveloper@gmail.com>
2025-07-02 21:55:53 +0530

    removed comments

--- src/main/java/com/springtechie/redis/services/ProductService.java
+++ src/main/java/com/springtechie/redis/services/ProductService.java
@@ -19,14 +19,6 @@ public class ProductService {
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
-    // client request with id
-    // it checks in the redis db with the given id.
-    // if data is present it return from redis.
-    // if not present then it goes to main db or actual db.
-    // just before returning the data to client , it update the redis db.
-    // {key : data}
-    // 120 : data
-
 
     @Cacheable(value = "productcache",key = "#id",unless = "#result==null",condition = "#id>10")
     public Product getProduct(int id) { // 1 : data
