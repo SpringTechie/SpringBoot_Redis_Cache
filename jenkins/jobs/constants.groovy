bc872cd8f78aa1958b3816c13ca03faa64e13f8e
springtechie <springframeworkdeveloper@gmail.com>
2025-07-07 20:57:30 +0530

    upodted

--- jenkins/jobs/constants.groovy
+++ jenkins/jobs/constants.groovy
@@ -1,6 +1,6 @@
 class Constants {
-    public static final REPO_URL = 'https://github.com/SpringTechie/SpringBoot_Redis_Cache.git';
-    public static final BRANCH = '*/main';
-    public static final MAVEN_TOOL = 'M3';
+     static final REPO_URL = 'https://github.com/SpringTechie/SpringBoot_Redis_Cache.git'
+     static final BRANCH = '*/main'
+     static final MAVEN_TOOL = 'M3'
 
 }
